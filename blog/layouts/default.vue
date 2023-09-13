e6d0a5837e100856e8b3cb51392ca43af9fedaf6
razdrayv <boryanspritefanta14@gmail.com>
2023-09-13 20:53:04 +0300

    13.09

--- blog/layouts/default.vue
+++ blog/layouts/default.vue
@@ -1,16 +1,45 @@
 <template>
   <v-app>
+    
+    <v-app-bar
+      app
+      color="grey"
+    >
+      <v-app-bar-title>
+        <h3 class="text-h4 white--text">iWear</h3>
+        <v-btn flat to="/" nuxt> Домой </v-btn>
+        <v-btn flat to="/about" nuxt> О нас </v-btn>
+        <v-btn to="/busket" nuxt>Корзина</v-btn>
+      </v-app-bar-title>
+    </v-app-bar>
+    
+    <v-main>
+      
+    </v-main>
 
+    <v-bottom-navigation :elevation="27" grow >
+  <v-btn value="recent">
+    <v-icon>mdi-history</v-icon>
 
-  <v-toolbar-items>
-    <v-btn flat to="/" nuxt> Home </v-btn>
-    <v-btn flat to="/about" nuxt> О нас </v-btn>
- </v-toolbar-items>
+    <span>Recent</span>
+  </v-btn>
 
-</v-app>
+  <v-btn value="favorites">
+    <v-icon>mdi-heart</v-icon>
+
+    <span>Favorites</span>
+  </v-btn>
+
+  <v-btn value="nearby">
+    <v-icon>mdi-map-marker</v-icon>
+
+    <span>Nearby</span>
+  </v-btn>
+</v-bottom-navigation>
+  </v-app>
 </template>
 
-<script>
+<script setup>
 
 </script>
 
